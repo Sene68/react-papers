c3840d9054efd48da803f31cac23783996489a65
Sene68 <senefull68@gmail.com>
2023-04-19 00:16:20 +0900

    메인 영역 디자인 작업, 메뉴1 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-violet-500 to-fuchsia-400 w-full h-screen">
+      <div className="border-2 border-white w-2/5 absolute translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 px-8 py-10">
+        <div className="relative bg-white px-8 py-5 rounded-md shadow-lg">
+          <div className="bg-white h-12 px-1 py-1.5 mt-1.5 flex items-center justify-between">
+              <span className="text-lg font-normal font-mono">Menu1</span>
+              <FontAwesomeIcon icon={faArrowRight} size="2xs" className='bg-violet-500 text-white h-full w-10 rounded-md border-0 cursor-pointer' />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
