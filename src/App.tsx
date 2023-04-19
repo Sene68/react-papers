7a95d8f7f78c361a1362f7e50b06d0011c3a375a
Sene68 <senefull68@gmail.com>
2023-04-19 18:24:22 +0900

    router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import ReactDOM from "react-dom/client";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { RandomColor } from "./pages/RandomColor";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-violet-500 to-fuchsia-400 w-full h-screen">
-      <div className="border-2 border-white w-2/5 absolute translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 px-8 py-10">
-        <div className="relative bg-white px-8 py-5 rounded-md shadow-lg">
-          <div className="bg-white h-12 px-1 py-1.5 mt-1.5 flex items-center justify-between">
-              <span className="text-lg font-normal font-mono">Menu1</span>
-              <FontAwesomeIcon icon={faArrowRight} size="2xs" className='bg-violet-500 text-white h-full w-10 rounded-md border-0 cursor-pointer' />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/random-color" element={<RandomColor />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+
+export const Home = () => {
+    return (
+        <div className="bg-gradient-to-r from-violet-500 to-fuchsia-400 w-full h-screen">
+            <div className="border-2 border-white w-2/5 absolute translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 px-8 py-10">
+                <div className="relative bg-white px-8 py-5 rounded-md shadow-lg">
+                    <div className="bg-white h-12 px-1 py-1.5 mt-1.5 flex items-center justify-between">
+                        <span className="text-lg font-normal font-mono">Random Color</span>
+                        <Link to="/random-color">
+                            <FontAwesomeIcon icon={faArrowRight} size="2xs" className='bg-violet-500 text-white h-full w-10 rounded-md border-0 cursor-pointer' />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
