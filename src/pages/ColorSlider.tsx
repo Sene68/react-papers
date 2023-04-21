7f3c714568474213301d5a01c3815e436d5e91e9
Sene68 <senefull68@gmail.com>
2023-04-22 02:20:11 +0900

    rgb 값 변경 함수 useCallback 사용

--- src/pages/ColorSlider.tsx
+++ src/pages/ColorSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 interface colorProps {
     r: number;
@@ -12,57 +12,32 @@ export const ColorSlider = () => {
         g: 0,
         b: 0
     })
-    
-    const [color, setColor] = useState({
-        backgroundColor: `rgb(0,0,0)`
-    })
 
     const [displayColor, setDisplayColor] = useState<string>('rgb (0, 0, 0)')
 
-    const changeR = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setColors({
-            ...colors,
-            r: parseInt(event.target.value)
-        })
-        setColor({
-            backgroundColor: `rgb(${event.target.value}, ${colors.g}, ${colors.b})`
-        })
-        setDisplayColor('rgb (' + event.target.value + ', ' + colors.g + ', ' + colors.b + ')')
-    }
-
-    const changeG = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setColors({
-            ...colors,
-            g: parseInt(event.target.value)
-        })
-        setColor({
-            backgroundColor: `rgb(${colors.r}, ${event.target.value}, ${colors.b})`
-        })
-        setDisplayColor('rgb (' + colors.r + ', ' + event.target.value + ', ' + colors.b + ')')
-    }
-
-    const changeB = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setColors({
-            ...colors,
-            b: parseInt(event.target.value)
-        })
-        setColor({
-            backgroundColor: `rgb(${colors.r}, ${colors.g}, ${event.target.value})`
-        })
-        setDisplayColor('rgb (' + colors.r + ', ' + colors.g + ', ' + event.target.value + ')')
-    }
+    const handleChangeRGB = useCallback(
+        (colorName: keyof colorProps, colorValue: number) => {
+            const newColors = {
+              ...colors,
+              [colorName]: colorValue,
+            };
+            setColors(newColors);
+            setDisplayColor(`rgb(${newColors.r}, ${newColors.g}, ${newColors.b})`);
+        },
+        [colors]
+    )
 
     return (
-        <div style={color} className="w-full h-screen">
+        <div style={{ backgroundColor: `rgb(${colors.r}, ${colors.g}, ${colors.b})` }} className="w-full h-screen">
             <div className="bg-white w-3/5 absolute translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 py-5 shadow-lg text-xl text-green-950 rounded-[5px] grid justify-items-center">
                 <div className="w-full text-center">
-                    R <input className="inline-block w-4/5 ml-2.5 mt-6" type="range" min={0} max={255} step={1} value={colors.r} onChange={changeR}/>
+                    R <input className="inline-block w-4/5 ml-2.5 mt-6" type="range" min={0} max={255} step={1} value={colors.r} onChange={(e) => handleChangeRGB("r", Number(e.target.value))}/>
                 </div>
                 <div className="w-full text-center">
-                    G <input className="inline-block w-4/5 ml-2.5 mt-6" type="range" min={0} max={255} step={1} value={colors.g} onInput={changeG}/>
+                    G <input className="inline-block w-4/5 ml-2.5 mt-6" type="range" min={0} max={255} step={1} value={colors.g} onChange={(e) => handleChangeRGB("g", Number(e.target.value))}/>
                 </div>
                 <div className="w-full text-center">
-                    B <input className="inline-block w-4/5 ml-2.5 mt-6" type="range" min={0} max={255} step={1} value={colors.b} onInput={changeB}/>
+                    B <input className="inline-block w-4/5 ml-2.5 mt-6" type="range" min={0} max={255} step={1} value={colors.b} onChange={(e) => handleChangeRGB("b", Number(e.target.value))}/>
                 </div>
                 <span className="inline-block text-center my-5 bg-blue-600 text-white py-5 px-8 text-xl tracking-[0.5px] rounded-[2px]">
                     {displayColor}
