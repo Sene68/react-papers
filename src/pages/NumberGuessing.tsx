a3a71ed54f4124628394791b4027585d33178c3f
Sene68 <senefull68@gmail.com>
2023-04-24 19:45:10 +0900

    게임 동작 구현

--- src/pages/NumberGuessing.tsx
+++ src/pages/NumberGuessing.tsx
@@ -1,7 +1,25 @@
+import React, { useState } from "react"
 
 export const NumberGuessing = () => {
-    const play = () => {
+    const [value, setValue] = useState<number>(0)
+    const [answer, setAnswer] = useState<number>(Math.floor(Math.random() * 100) + 1)
+    const [message, setMessage] = useState<string>('')
+    const [guessedNums, setGuessedNums] = useState<number[]>([])
 
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(parseInt(e.target.value))
+    }
+
+    const play = () => {
+        setGuessedNums([...guessedNums, value])
+
+        if (value < answer) {
+            setMessage('Your guess is too low.')
+        } else if (value > answer) {
+            setMessage('Your guess is too high.')
+        } else if (value === answer) {
+            setMessage('Yipple You Win!!')
+        }
     }
 
     return(
@@ -10,13 +28,21 @@ export const NumberGuessing = () => {
                 <h3 className="font-mono text-base font-semibold">I am thinking of a number between 1-100.</h3>
                 <h3 className="font-mono text-base font-semibold">Can you guess it?</h3>
 
-                <input className="font-mono w-24 font-semibold text-purple-800 py-5 text-center mt-8 rounded-md border-2 border-solid border-gray-700" type="text" placeholder="Num"/>
+                <input 
+                    className="font-mono w-24 font-semibold text-purple-800 py-5 text-center mt-8 rounded-md border-2 border-solid border-gray-700" 
+                    type="text"
+                    placeholder="Num"
+                    pattern="[0-9]*"
+                    onChange={onChange}
+                    value={value}
+                />
                 <br/>
                 <button className="font-mono w-40 py-4 bg-violet-800 text-white border-none rounded-md text-lg font-semibold mb-8" onClick={play}>GUESS</button>
 
-                <p className="font-mono">No. of Guesses: 0</p>
-                <p className="font-mono">Guessed Numbers are: None</p>
-                <p className="font-mono"></p>
+                <p className="font-mono">{message}</p>
+                <p className="font-mono">No. of Guesses: {guessedNums.length}</p>
+                <p className="font-mono">Guessed Numbers are: {value}</p>
+                
             </div>
         </div>
     )
