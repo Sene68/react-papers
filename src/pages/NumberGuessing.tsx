f1bd8afae594820aef3279d0b8ead4d4e5ef2b56
Sene68 <senefull68@gmail.com>
2023-04-25 19:20:33 +0900

    useReducer 적용

--- src/pages/NumberGuessing.tsx
+++ src/pages/NumberGuessing.tsx
@@ -1,27 +1,60 @@
-import React, { useState } from "react"
+import React, { useState, useReducer } from "react"
+
+interface State {
+    value: number;
+    message: string;
+    answer: number;
+}
+
+type Action =
+  | { type: "SET_VALUE"; payload: number }
+  | { type: "SET_MESSAGE"; payload: string }
+  | { type: "SET_ANSWER"; payload: number };
+
+
+const initialState: State = {
+    value: 0,
+    message: "",
+    answer: Math.floor(Math.random() * 100) + 1,
+};
+
+const reducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case "SET_VALUE":
+            return { ...state, value: action.payload };
+        case "SET_MESSAGE":
+            return { ...state, message: action.payload };
+        case "SET_ANSWER":
+            return { ...state, answer: action.payload };
+        default:
+            return state;
+    }
+}
 
 export const NumberGuessing = () => {
-    const [value, setValue] = useState<number>(0)
-    const [answer, setAnswer] = useState<number>(Math.floor(Math.random() * 100) + 1)
-    const [message, setMessage] = useState<string>('')
     const [guessedNums, setGuessedNums] = useState<number[]>([])
 
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(parseInt(e.target.value))
+        const value = parseInt(e.target.value)
+        dispatch({ type: "SET_VALUE", payload: value });
     }
 
     const play = () => {
-        setGuessedNums([...guessedNums, value])
+        const { value, answer } = state;
 
+        setGuessedNums([...guessedNums, value]) //이 영역도 reducer로 처리하면댐
         if (value < answer) {
-            setMessage('Your guess is too low.')
+            dispatch({ type: "SET_MESSAGE", payload: "Your guess is too low." });
         } else if (value > answer) {
-            setMessage('Your guess is too high.')
-        } else if (value === answer) {
-            setMessage('Yipple You Win!!')
+            dispatch({ type: "SET_MESSAGE", payload: "Your guess is too high." });
+        } else {
+            dispatch({ type: "SET_MESSAGE", payload: "Yipple You Win!!" });
         }
     }
 
+    // Reset 추가도 필요
     return(
         <div className="bg-gradient-to-r from-violet-500 to-indigo-400 w-full h-screen">
             <div className="absolute w-1/2 min-w-[580px] translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 bg-white px-2.5 py-12 rounded-md grid justify-items-center">
@@ -34,14 +67,14 @@ export const NumberGuessing = () => {
                     placeholder="Num"
                     pattern="[0-9]*"
                     onChange={onChange}
-                    value={value}
+                    value={state.value}
                 />
                 <br/>
                 <button className="font-mono w-40 py-4 bg-violet-800 text-white border-none rounded-md text-lg font-semibold mb-8" onClick={play}>GUESS</button>
 
-                <p className="font-mono">{message}</p>
+                <p className="font-mono">{state.message}</p>
                 <p className="font-mono">No. of Guesses: {guessedNums.length}</p>
-                <p className="font-mono">Guessed Numbers are: {value}</p>
+                <p className="font-mono">Guessed Numbers are: {state.value}</p>
                 
             </div>
         </div>
