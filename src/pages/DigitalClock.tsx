438a25ee4b56e2d18c709b9d4b13bfd1eacae135
Sene68 <senefull68@gmail.com>
2023-04-30 01:11:53 +0900

    시간 타입 지정 및 변수 변경

--- src/pages/DigitalClock.tsx
+++ src/pages/DigitalClock.tsx
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react"
 
+interface ClockProps {
+    hours: number;
+    minutes: number;
+    seconds: number;
+}
+
 export const DigitalClock = () => {
-    const [hours, setHours] = useState(0)
-    const [minutes, setMinutes] = useState(0)
-    const [seconds, setSeconds] = useState(0)
+    const [clock, setClock] = useState<ClockProps>({
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    })
 
     useEffect(() => {
         const interval = setInterval(()=> {
             const now = new Date()
-            setHours(now.getHours())
-            setMinutes(now.getMinutes())
-            setSeconds(now.getSeconds())
+            setClock(clock => ({
+                ...clock,
+                hours: now.getHours(),
+                minutes: now.getMinutes(),
+                seconds: now.getSeconds()
+            }))
         }, 1000)
         return () => clearInterval(interval)
     }, [])
@@ -18,11 +29,11 @@ export const DigitalClock = () => {
     return(
         <div className="bg-gradient-to-r from-violet-500 to-indigo-400 w-full h-screen">
             <div className="w-[550px] h-[150px] absolute translate-x-[-50%] translate-y-[-50%] top-1/2 left-1/2 flex items-center justify-between">
-                <div className="relative bg-white h-full w-[150px] flex items-center justify-center text-6xl rounded-md shadow-lg tracking-[3px]">{hours}</div>
+                <div className="relative bg-white h-full w-[150px] flex items-center justify-center text-6xl rounded-md shadow-lg tracking-[3px]">{clock.hours}</div>
                 <span className="font-bold text-6xl text-white">:</span>
-                <div className="relative bg-white h-full w-[150px] flex items-center justify-center text-6xl rounded-md shadow-lg tracking-[3px]">{minutes}</div>
+                <div className="relative bg-white h-full w-[150px] flex items-center justify-center text-6xl rounded-md shadow-lg tracking-[3px]">{clock.minutes}</div>
                 <span className="font-bold text-6xl text-white">:</span>
-                <div className="relative bg-white h-full w-[150px] flex items-center justify-center text-6xl rounded-md shadow-lg tracking-[3px]">{seconds}</div>
+                <div className="relative bg-white h-full w-[150px] flex items-center justify-center text-6xl rounded-md shadow-lg tracking-[3px]">{clock.seconds}</div>
             </div>
         </div>
     )
