01bdf25d89c8a20b637bf95e68a5ad61456671db
Sene68 <senefull68@gmail.com>
2023-05-22 09:47:58 +0900

    슬라이드 버튼 클릭 시 이미지 이동

--- src/pages/ResponsiveImageSlider.tsx
+++ src/pages/ResponsiveImageSlider.tsx
@@ -28,9 +28,21 @@ export const ResponsiveImageSlider = () => {
                     <img style={{display: currentIndex === 2 ? "block" : "none"}} className="relative w-full" src={slider_img_3} alt=""/>
                 </div>
                 <div className="w-[150px] mt-[20px] flex items-center justify-around">
-                    <button style={{backgroundColor: currentIndex === 0 ? "#8052ec" : "transparent"}} className="w-[13px] h-[13px] rounded-full border-[3px] border-solid border-[#8052ec]"></button>
-                    <button style={{backgroundColor: currentIndex === 1 ? "#8052ec" : "transparent"}} className="w-[13px] h-[13px] rounded-full border-[3px] border-solid border-[#8052ec]"></button>
-                    <button style={{backgroundColor: currentIndex === 2 ? "#8052ec" : "transparent"}} className="w-[13px] h-[13px] rounded-full border-[3px] border-solid border-[#8052ec]"></button>
+                    <button 
+                        onClick={() => setCurrentIndex(0)}
+                        style={{backgroundColor: currentIndex === 0 ? "#8052ec" : "transparent"}} 
+                        className="w-[13px] h-[13px] rounded-full border-[3px] border-solid border-[#8052ec]">     
+                    </button>
+                    <button 
+                        onClick={() => setCurrentIndex(1)}
+                        style={{backgroundColor: currentIndex === 1 ? "#8052ec" : "transparent"}} 
+                        className="w-[13px] h-[13px] rounded-full border-[3px] border-solid border-[#8052ec]">
+                    </button>
+                    <button 
+                        onClick={() => setCurrentIndex(2)}
+                        style={{backgroundColor: currentIndex === 2 ? "#8052ec" : "transparent"}} 
+                        className="w-[13px] h-[13px] rounded-full border-[3px] border-solid border-[#8052ec]">
+                    </button>
                 </div>
                 <button 
                     onClick={() => prev()}
